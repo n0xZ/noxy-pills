19b2cd8d14587f81d99e6f7dfc30a18d487def8d
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-08-22 13:27:38 -0300

    feat: add import aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
-import Unocss from 'unocss/vite'
+import { defineConfig } from "vite";
+import solid from "vite-plugin-solid";
+import Unocss from "unocss/vite";
+import { resolve } from "path";
 export default defineConfig({
-	plugins: [solid(), Unocss()],
-})
+  plugins: [solid(), Unocss()],
+  resolve: { alias: { "~": resolve(__dirname, "src") } },
+});
