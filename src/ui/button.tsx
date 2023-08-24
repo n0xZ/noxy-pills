7b9019e0ff81c4c74118ffc1d65d63f5ee87309c
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-08-24 16:20:21 -0300

    chore: override button class prop

--- src/ui/button.tsx
+++ src/ui/button.tsx
@@ -1,14 +1,14 @@
-import { JSX } from "solid-js";
+import { JSX, splitProps } from "solid-js";
 
 interface Props extends JSX.ButtonHTMLAttributes<HTMLButtonElement> {
   children: JSX.Element | string;
 }
 export function Button(props: Props) {
+  const [c, rest] = splitProps(props, ["class"]);
+  const baseStyles =
+    "inline-flex items-center p-3 w-full text-black justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50";
   return (
-    <button
-      {...props}
-      class="inline-flex items-center p-3 w-full text-black justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
-    >
+    <button {...rest} class={`${baseStyles} ${c}`}>
       {props.children}
     </button>
   );
