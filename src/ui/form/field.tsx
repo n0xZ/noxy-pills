66743ade7c70b4110c04d62c44b6ef84d148641f
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-08-24 16:16:51 -0300

    remove default import

--- src/ui/form/field.tsx
+++ src/ui/form/field.tsx
@@ -3,7 +3,7 @@ import { JSX } from "solid-js";
 type Props = {
   children?: JSX.Element;
 };
-export default function FormField(props: Props) {
+export  function FormField(props: Props) {
   return (
     <aside class="flex flex-col space-y-2 justify-center w-full xl:max-w-2xl lg:max-w-lg md:max-w-lg ">
       {props.children}
