181fa6241d7cb8fe0f5fefe84212d5d0f3a0f31f
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-08-24 16:17:00 -0300

    feat: add query provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,23 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
 import { Router } from "@solidjs/router";
+import { MetaProvider } from "@solidjs/meta";
+import { QueryClient, QueryClientProvider } from "@tanstack/solid-query";
 import "virtual:uno.css";
 import "@unocss/reset/tailwind-compat.css";
 import App from "./App";
 
 const root = document.getElementById("root");
-
+const client = new QueryClient();
 render(
   () => (
-    <Router>
-      <App />
-    </Router>
+    <QueryClientProvider client={client}>
+      <MetaProvider>
+        <Router>
+          <App />
+        </Router>
+      </MetaProvider>
+    </QueryClientProvider>
   ),
   root!
 );
