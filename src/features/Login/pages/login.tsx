1d4b67711708b949fba90c02511fe742c331fc8b
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-08-26 10:40:15 -0300

    feat: add login page

--- src/features/Login/pages/login.tsx
+++ src/features/Login/pages/login.tsx
@@ -1,63 +1,25 @@
 import { Title } from "@solidjs/meta";
-import { Link, useNavigate } from "@solidjs/router";
-import { Show, createSignal } from "solid-js";
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { ValiError, safeParse } from "valibot";
+import { Link } from "@solidjs/router";
+import { Show } from "solid-js";
+
 import { Button } from "~/ui/button";
 import { FormField } from "~/ui/form/field";
 import { Input } from "~/ui/form/input";
 import { Label } from "~/ui/form/label";
-import { auth } from "~/lib/firebase";
-import { credentialsSchema } from "~/utils/valibot";
+
 import { handleFirebaseErrors } from "~/utils/firebase-errors";
+import { createAuth } from "~/features/Primitives/auth";
 
-type Errors = {
-  authErrors?: string;
-  formErrors: ValiError;
-};
 export default function Login() {
-  const [isSubmitting, setIsSubmitting] = createSignal(false);
-  const [errors, setErrors] = createSignal<Errors>({} as Errors);
-  const navigate = useNavigate();
-  const onSubmit = async (
-    e: Event & { submitter: HTMLElement } & {
-      currentTarget: HTMLFormElement;
-      target: Element;
-    }
-  ) => {
-    try {
-      e.preventDefault();
-      setIsSubmitting(true);
-      const formData = new FormData(e.currentTarget);
-      const fields = Object.fromEntries(formData);
-      const formResult = safeParse(credentialsSchema, fields);
+  const { containsFormErrors, errorFromField, signInMutation, signIn } =
+    createAuth();
 
-      console.log(formResult);
-      if (!formResult.success) {
-        setIsSubmitting(false);
-        setErrors({ ...errors(), formErrors: formResult.error });
-      } else {
-        const { user } = await signInWithEmailAndPassword(
-          auth,
-          formResult.data.email,
-          formResult.data.password
-        );
-
-        if (user) navigate("/home");
-      }
-    } catch (e) {
-      setIsSubmitting(false);
-      if (e instanceof Error) {
-        setErrors({ ...errors(), authErrors: e.message });
-      }
-    }
-  };
   return (
     <main class="h-screen">
       <Title>Solyx - Iniciar sesión</Title>
       <article class="grid place-items-center  w-full h-full">
         <form
-          onSubmit={onSubmit}
+          onSubmit={signIn}
           class="  xl:p-0 p-3 flex flex-col items-center justify-center items-center w-full   xl:max-w-2xl lg:max-w-lg md:max-w-lg container mx-auto space-y-5  "
         >
           <h2 class="text-center xl:text-3xl text-lg font-bold mb-3">
@@ -69,32 +31,48 @@ export default function Login() {
               autocomplete="off"
               type="email"
               name="email"
+              disabled={signInMutation.isLoading}
               required
               placeholder="o.gonzalo1232131@miemail.com"
               data-test-id="email-input"
             />
+            <span class="text-xs h-4 text-red-500" data-test-id="auth-errors">
+              <Show when={containsFormErrors("email")}>
+                {errorFromField("email")}
+              </Show>
+            </span>
           </FormField>
-          <Input
-            type="password"
-            name="password"
-            required
-            placeholder="gonzalo12313*"
-            data-test-id="password-input"
-          />
+          <FormField>
+            <Label>Contraseña</Label>
+            <Input
+              type="password"
+              name="password"
+              required
+              disabled={signInMutation.isLoading}
+              placeholder="gonzalo12313*"
+              data-test-id="password-input"
+            />
+            <span class="text-xs h-4 text-red-500" data-test-id="auth-errors">
+              <Show when={containsFormErrors("email")}>
+                {errorFromField("email")}
+              </Show>
+            </span>
+          </FormField>
+
           <Button
             class={`px-8 py-4 ${
-              isSubmitting() ? "bg-emerald-600" : "bg-emerald-500"
-            } font-bold w-full  text-light-500 rounded-lg max-w-2xl`}
+              signInMutation.isLoading ? "bg-teal-600" : "bg-teal-500"
+            } font-bold w-full  c-light-500 rounded-lg max-w-2xl`}
             type="submit"
             data-test-id="submit-login"
-            disabled={isSubmitting()}
+            disabled={signInMutation.isLoading}
           >
-            {isSubmitting() ? "Iniciando..." : "Iniciar sesión"}
+            {signInMutation.isLoading ? "Iniciando..." : "Iniciar sesión"}
           </Button>
 
-          <span class="h-6 text-red-500" data-test-id="auth-errors">
-            <Show when={errors().authErrors}>
-              {handleFirebaseErrors(errors().authErrors)}
+          <span class="text-xs h-4 text-red-500" data-test-id="auth-errors">
+            <Show when={signInMutation}>
+              {handleFirebaseErrors(signInMutation?.error?.message)}
             </Show>
           </span>
 
