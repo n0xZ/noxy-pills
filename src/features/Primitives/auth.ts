7ea972c41afcad47541a32b90dbe2ffd4e02c070
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-08-26 10:27:56 -0300

    feat: signUp mutation

--- src/features/Primitives/auth.ts
+++ src/features/Primitives/auth.ts
@@ -4,18 +4,36 @@ import { createMutation } from "@tanstack/solid-query";
 import { createSignal } from "solid-js";
 import { ValiError, safeParse } from "valibot";
 
-import { signInViaEmail } from "~/services/auth";
+import { signInViaEmail, signUpViaEmail } from "~/services/auth";
 import { CredentialsOutput, credentialsSchema } from "~/utils/valibot";
 
 export const createAuth = () => {
   const [formErrors, setFormErrors] = createSignal<ValiError>({} as ValiError);
-  const { data, mutate, isLoading, error } = createMutation(signInViaEmail, {
+  const {
+    data: signInData,
+    mutate: mutateSignIn,
+    isLoading: isSignInLoading,
+    error: signInError,
+  } = createMutation(signInViaEmail, {
+    onError(e: Error) {
+      return e.message;
+    },
+  });
+  const {
+    data: signUpData,
+    mutate: mutateSignUp,
+    isLoading: isSignUpLoading,
+    error: signUpError,
+  } = createMutation(signUpViaEmail, {
     onError(e: Error) {
       return e.message;
     },
   });
   const navigate = useNavigate();
 
+  /**
+This should work
+ */
   const containsFormErrors = (key: keyof CredentialsOutput) =>
     formErrors().issues &&
     formErrors().issues.some((i) => i.path?.[0].key === key);
@@ -40,16 +58,38 @@ export const createAuth = () => {
     if (!formResult.success) {
       setFormErrors({ ...formErrors(), formErrors: formResult.error });
     } else {
-      mutate(formResult.data);
-      if (data) navigate("/home");
+      mutateSignIn(formResult.data);
+      if (signInData) navigate("/home");
     }
   };
+  const signUp = async (
+    e: Event & { submitter: HTMLElement } & {
+      currentTarget: HTMLFormElement;
+      target: Element;
+    }
+  ) => {
+    e.preventDefault();
 
+    const formData = new FormData(e.currentTarget);
+    const fields = Object.fromEntries(formData);
+    const formResult = safeParse(credentialsSchema, fields);
+
+    if (!formResult.success) {
+      setFormErrors({ ...formErrors(), formErrors: formResult.error });
+    } else {
+      mutateSignUp(formResult.data);
+      if (signUpData) navigate("/home");
+    }
+  };
   return {
     signIn,
-    isLoading,
-    authErrors: error,
+    isSignInLoading,
+    signInAuthErrors: signInError,
     containsFormErrors,
     errorFromField,
+    signUp,
+    isSignUpLoading,
+    signUpAuthErrors: signUpError,
+    signUpError,
   };
 };
