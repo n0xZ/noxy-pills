7ca55fe45aa2f6f0eeed471ff260139e3b14e811
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-08-27 18:33:03 -0300

    feat: add solid-firebase dep

--- src/index.tsx
+++ src/index.tsx
@@ -2,22 +2,26 @@
 import { render } from "solid-js/web";
 import { Router } from "@solidjs/router";
 import { MetaProvider } from "@solidjs/meta";
+import { FirebaseProvider } from "solid-firebase";
 import { QueryClient, QueryClientProvider } from "@tanstack/solid-query";
 import "virtual:uno.css";
 import "@unocss/reset/tailwind-compat.css";
 import App from "./App";
 
+import { firebase } from "./lib/firebase";
 const root = document.getElementById("root");
 const client = new QueryClient();
 render(
   () => (
-    <QueryClientProvider client={client}>
-      <MetaProvider>
-        <Router>
-          <App />
-        </Router>
-      </MetaProvider>
-    </QueryClientProvider>
+    <FirebaseProvider app={firebase}>
+      <QueryClientProvider client={client}>
+        <MetaProvider>
+          <Router>
+            <App />
+          </Router>
+        </MetaProvider>
+      </QueryClientProvider>
+    </FirebaseProvider>
   ),
   root!
 );
