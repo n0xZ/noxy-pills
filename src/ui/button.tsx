407cb9d6abd65ec4102d0945971c617041ff8ac7
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-08-27 18:33:33 -0300

    fix: spread rest props issue (disabled & overrided class were not applied on element)

--- src/ui/button.tsx
+++ src/ui/button.tsx
@@ -4,11 +4,15 @@ interface Props extends JSX.ButtonHTMLAttributes<HTMLButtonElement> {
   children: JSX.Element | string;
 }
 export function Button(props: Props) {
-  const [c, rest] = splitProps(props, ["class"]);
+  const [splittedProps, rest] = splitProps(props, ["class", "disabled"]);
   const baseStyles =
-    "inline-flex items-center p-3 w-full text-black justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50";
+    "inline-flex items-center p-3 w-full  justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50";
   return (
-    <button {...rest} class={`${baseStyles} ${c}`}>
+    <button
+      disabled={splittedProps.disabled}
+      {...rest}
+      class={`${baseStyles} ${splittedProps.class}`}
+    >
       {props.children}
     </button>
   );
