7dbbc81d34e5bc017dcc8202f5c6fc9966bd4fe9
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-08-27 12:59:00 -0300

    feat: add register page

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { Routes, Route } from "@solidjs/router";
 import { lazy } from "solid-js";
 const Home = lazy(() => import("./features/Landing/pages/landing"));
 const Login = lazy(() => import("./features/Login/pages/login"));
+const Register = lazy(() => import("./features/Register/pages/register"));
 function App() {
   return (
     <Routes>
       <Route path="/" component={Home} />
       <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
     </Routes>
   );
 }
