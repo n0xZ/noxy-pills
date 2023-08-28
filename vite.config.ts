59f6242040d1a4c10629ceec1283a944f60f2187
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-08-28 14:42:21 -0300

    feat: add inter webfont from bunnyfonts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
 import Unocss from "unocss/vite";
+import { presetWebFonts, presetUno } from "unocss";
 import { resolve } from "path";
+
 export default defineConfig({
-  plugins: [solid(), Unocss()],
+  plugins: [
+    solid(),
+    Unocss({
+      presets: [
+        presetUno(),
+        presetWebFonts({
+          provider: "bunny",
+          fonts: {
+            inter: ["Inter", "sans-serif"],
+          },
+        }),
+      ],
+    }),
+  ],
   resolve: { alias: { "~": resolve(__dirname, "src") } },
 });
