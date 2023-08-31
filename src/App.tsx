6ae59f534acd9959fad656cec3f253ecd83ff52d
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-08-31 11:06:08 -0300

    feat: home routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,17 @@ import { lazy } from "solid-js";
 const Home = lazy(() => import("./features/Landing/pages/landing"));
 const Login = lazy(() => import("./features/Login/pages/login"));
 const Register = lazy(() => import("./features/Register/pages/register"));
+const HomeOutlet = lazy(() => import("./features/Home/pages/home.outlet"));
+const HomeGeneral = lazy(() => import("./features/Home/pages/index"));
 function App() {
   return (
     <Routes>
       <Route path="/" component={Home} />
       <Route path="/login" component={Login} />
       <Route path="/register" component={Register} />
+      <Route path="/home" component={HomeOutlet} >
+        <Route path="/" component={HomeGeneral} />
+      </Route>
     </Routes>
   );
 }
