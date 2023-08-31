4be3a9b205dab8459c34e0d4c8b6d792e146e693
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-08-31 11:04:51 -0300

    feat: add pill schema

--- src/utils/valibot.ts
+++ src/utils/valibot.ts
@@ -1,8 +1,25 @@
-import { object, string, email,Output } from "valibot";
+import {
+	object,
+	string,
+	email,
+	Output,
+	array,
+	number,
+	date,
+	parse,
+	any,
+} from "valibot";
 
 export const credentialsSchema = object({
-  email: string([email("Campo requerido")]),
-  password: string(),
+	email: string([email("Campo requerido")]),
+	password: string(),
 });
-
-export type CredentialsOutput = Output<typeof credentialsSchema>
+export const pillSchema = object({
+	name: string(),
+	description: string(),
+	userId: string(),
+	duration: number(),
+	createdAt: any(),
+});
+export type PillsOutput = Output<typeof pillSchema>;
+export type CredentialsOutput = Output<typeof credentialsSchema>;
