6cd52c9d635a7b6910beb19100993eb2a7f827d2
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-09-11 16:22:19 -0300

    feat: add vite-pwa integration

--- vite.config.ts
+++ vite.config.ts
@@ -2,22 +2,24 @@ import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
 import Unocss from "unocss/vite";
 import { presetWebFonts, presetUno } from "unocss";
+import { VitePWA } from "vite-plugin-pwa";
 import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [
-    solid(),
-    Unocss({
-      presets: [
-        presetUno(),
-        presetWebFonts({
-          provider: "bunny",
-          fonts: {
-            inter: ["Inter", "sans-serif"],
-          },
-        }),
-      ],
-    }),
-  ],
-  resolve: { alias: { "~": resolve(__dirname, "src") } },
+	plugins: [
+		solid(),
+		Unocss({
+			presets: [
+				presetUno(),
+				presetWebFonts({
+					provider: "bunny",
+					fonts: {
+						inter: ["Inter", "sans-serif"],
+					},
+				}),
+			],
+		}),
+		VitePWA({ injectRegister: "auto" }),
+	],
+	resolve: { alias: { "~": resolve(__dirname, "src") } },
 });
