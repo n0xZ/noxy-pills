db1c1dd9167ff73d9585f5bdf96a444101c0fc70
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-09-11 16:27:50 -0300

    feat: add pill list skeleton to display on load

---
+++ src/features/Home/components/pills/skeleton.tsx
@@ -0,0 +1,83 @@
+export function PillSkeletonList() {
+	return (
+		<section class="grid grid-rows-3 container mx-auto max-w-3xl h-full gap-5  ">
+			<article class="flex flex-col justify-center space-y-1  px-3 py-2 w-full border-b-2 ">
+				<aside class="flex flex-row items-center justify-between">
+					<div class="w-36 animate-pulse p-2 rounded-md bg-gray-300" />
+					<div class="h-10 w-10 animate-pulse p-2 rounded-xl bg-gray-300" />
+				</aside>
+				<div class="p-1 w-24 rounded-sm bg-gray-300 animate-pulse" />
+				<span class="flex xl:flex-row flex-col items-center justify-between ">
+					<div class="w-24 animate-pulse p-2 rounded-md bg-gray-300" />
+					<div class="w-36 animate-pulse p-2 rounded-md bg-gray-300" />
+				</span>
+			</article>
+			<article class="flex flex-col justify-center space-y-1  px-3 py-2 w-full border-b-2 ">
+				<aside class="flex flex-row items-center justify-between">
+					<div class="w-36 animate-pulse p-2 rounded-md bg-gray-300" />
+					<div class="h-10 w-10 animate-pulse p-2 rounded-xl bg-gray-300" />
+				</aside>
+				<div class="p-1 w-24 rounded-sm bg-gray-300 animate-pulse" />
+				<span class="flex xl:flex-row flex-col items-center justify-between ">
+					<div class="w-24 animate-pulse p-2 rounded-md bg-gray-300" />
+					<div class="w-36 animate-pulse p-2 rounded-md bg-gray-300" />
+				</span>
+			</article>
+			<article class="flex flex-col justify-center space-y-1  px-3 py-2 w-full border-b-2 ">
+				<aside class="flex flex-row items-center justify-between">
+					<div class="w-36 animate-pulse p-2 rounded-md bg-gray-300" />
+					<div class="h-10 w-10 animate-pulse p-2 rounded-xl bg-gray-300" />
+				</aside>
+				<div class="p-1 w-24 rounded-sm bg-gray-300 animate-pulse" />
+				<span class="flex xl:flex-row flex-col items-center justify-between ">
+					<div class="w-24 animate-pulse p-2 rounded-md bg-gray-300" />
+					<div class="w-36 animate-pulse p-2 rounded-md bg-gray-300" />
+				</span>
+			</article>
+			<article class="flex flex-col justify-center space-y-1  px-3 py-2 w-full border-b-2 ">
+				<aside class="flex flex-row items-center justify-between">
+					<div class="w-36 animate-pulse p-2 rounded-md bg-gray-300" />
+					<div class="h-10 w-10 animate-pulse p-2 rounded-xl bg-gray-300" />
+				</aside>
+				<div class="p-1 w-24 rounded-sm bg-gray-300 animate-pulse" />
+				<span class="flex xl:flex-row flex-col items-center justify-between ">
+					<div class="w-24 animate-pulse p-2 rounded-md bg-gray-300" />
+					<div class="w-36 animate-pulse p-2 rounded-md bg-gray-300" />
+				</span>
+			</article>
+			<article class="flex flex-col justify-center space-y-1  px-3 py-2 w-full border-b-2 ">
+				<aside class="flex flex-row items-center justify-between">
+					<div class="w-36 animate-pulse p-2 rounded-md bg-gray-300" />
+					<div class="h-10 w-10 animate-pulse p-2 rounded-xl bg-gray-300" />
+				</aside>
+				<div class="p-1 w-24 rounded-sm bg-gray-300 animate-pulse" />
+				<span class="flex xl:flex-row flex-col items-center justify-between ">
+					<div class="w-24 animate-pulse p-2 rounded-md bg-gray-300" />
+					<div class="w-36 animate-pulse p-2 rounded-md bg-gray-300" />
+				</span>
+			</article>
+			<article class="flex flex-col justify-center space-y-1  px-3 py-2 w-full border-b-2 ">
+				<aside class="flex flex-row items-center justify-between">
+					<div class="w-36 animate-pulse p-2 rounded-md bg-gray-300" />
+					<div class="h-10 w-10 animate-pulse p-2 rounded-xl bg-gray-300" />
+				</aside>
+				<div class="p-1 w-24 rounded-sm bg-gray-300 animate-pulse" />
+				<span class="flex xl:flex-row flex-col items-center justify-between ">
+					<div class="w-24 animate-pulse p-2 rounded-md bg-gray-300" />
+					<div class="w-36 animate-pulse p-2 rounded-md bg-gray-300" />
+				</span>
+			</article>
+			<article class="flex flex-col justify-center space-y-1  px-3 py-2 w-full border-b-2 ">
+				<aside class="flex flex-row items-center justify-between">
+					<div class="w-36 animate-pulse p-2 rounded-md bg-gray-300" />
+					<div class="h-10 w-10 animate-pulse p-2 rounded-xl bg-gray-300" />
+				</aside>
+				<div class="p-1 w-24 rounded-sm bg-gray-300 animate-pulse" />
+				<span class="flex xl:flex-row flex-col items-center justify-between ">
+					<div class="w-24 animate-pulse p-2 rounded-md bg-gray-300" />
+					<div class="w-36 animate-pulse p-2 rounded-md bg-gray-300" />
+				</span>
+			</article>
+		</section>
+	);
+}
