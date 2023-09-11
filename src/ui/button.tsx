41196ebea58de27141483bf488fb2c912db20778
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-09-11 12:57:09 -0300

    fix: set button type class instead of spreading it

--- src/ui/button.tsx
+++ src/ui/button.tsx
@@ -1,19 +1,24 @@
 import { JSX, splitProps } from "solid-js";
 
 interface Props extends JSX.ButtonHTMLAttributes<HTMLButtonElement> {
-  children: JSX.Element | string;
+	children: JSX.Element | string;
 }
 export function Button(props: Props) {
-  const [splittedProps, rest] = splitProps(props, ["class", "disabled"]);
-  const baseStyles =
-    "inline-flex items-center p-3 w-full  justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50";
-  return (
-    <button
-      disabled={splittedProps.disabled}
-      {...rest}
-      class={`${baseStyles} ${splittedProps.class}`}
-    >
-      {props.children}
-    </button>
-  );
+	const [splittedProps, rest] = splitProps(props, [
+		"class",
+		"disabled",
+		"type",
+	]);
+	const baseStyles =
+		"inline-flex items-center p-3   justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50";
+	return (
+		<button
+			type={splittedProps.type}
+			disabled={splittedProps.disabled}
+			{...rest}
+			class={`${baseStyles} ${splittedProps.class}`}
+		>
+			{props.children}
+		</button>
+	);
 }
