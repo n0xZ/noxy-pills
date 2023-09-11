80abf48b7b2f7ab0a1de736065cbd923a2702cc5
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-09-11 10:04:11 -0300

    fix: replace ternary jsx on Button with Show component

--- src/features/Login/pages/login.tsx
+++ src/features/Login/pages/login.tsx
@@ -19,6 +19,7 @@ function SpinnerIcon() {
 			viewBox="0 0 24 24"
 			class="h-5 w-5"
 		>
+			<title>Spinner icon</title>
 			<path
 				fill="currentColor"
 				d="M12,4a8,8,0,0,1,7.89,6.7A1.53,1.53,0,0,0,21.38,12h0a1.5,1.5,0,0,0,1.48-1.75,11,11,0,0,0-21.72,0A1.5,1.5,0,0,0,2.62,12h0a1.53,1.53,0,0,0,1.49-1.3A8,8,0,0,1,12,4Z"
@@ -92,14 +93,12 @@ export default function Login() {
 						data-test-id="submit-login"
 						disabled={signInMutation.isLoading}
 					>
-						{signInMutation.isLoading ? (
+						<Show when={signInMutation.isLoading} fallback="Iniciar sesión">
 							<>
 								<SpinnerIcon />
 								<span>Iniciando...</span>
 							</>
-						) : (
-							"Iniciar sesión"
-						)}
+						</Show>
 					</Button>
 
 					<span class="text-xs h-4 text-red-500" data-test-id="auth-errors">
