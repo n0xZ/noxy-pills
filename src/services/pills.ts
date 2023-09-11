dcad9904850f44cea4c861175fe64b67743b7f73
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-09-11 10:05:54 -0300

    feat: add createPill service

--- src/services/pills.ts
+++ src/services/pills.ts
@@ -1,8 +1,16 @@
-import { collection, getDocs, query, where } from "firebase/firestore";
+import {
+	addDoc,
+	collection,
+	doc,
+	getDocs,
+	query,
+	setDoc,
+	where,
+} from "firebase/firestore";
 import { parse } from "valibot";
 import { firestore } from "~/lib/firebase";
 
-import { pillSchema } from "~/utils/valibot";
+import { PillsOutput, pillSchema } from "~/utils/valibot";
 
 export const getPillsByUserId = async (userId: string) => {
 	try {
@@ -14,6 +22,31 @@ export const getPillsByUserId = async (userId: string) => {
 		const pillsDocs = pillsCollection.docs;
 		const pillsData = pillsDocs.map((doc) => parse(pillSchema, doc.data()));
 		return pillsData;
+	} catch (e) {
+		if (e instanceof Error) {
+			throw new Error(e.message);
+		}
+	}
+};
+
+export const createPill = async ({
+	description,
+	duration,
+	frequency,
+	name,
+	userId,
+}: PillsOutput) => {
+	try {
+		const pillColl = collection(firestore, "pills");
+		const createPill = await addDoc(pillColl, {
+			description,
+			duration,
+			frequency,
+			name,
+			userId,
+			createdAt: new Date(),
+		});
+		return createPill;
 	} catch (e) {
 		if (e instanceof Error) {
 			throw new Error(e.message);
