5312d891059b0cf31886373f1830d3ee65725fd4
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-09-18 17:45:52 -0300

    feat: add skeleton list comp.

--- src/features/Home/pages/index.tsx
+++ src/features/Home/pages/index.tsx
@@ -1,27 +1,29 @@
+import { Match, Switch } from "solid-js";
 import { Title } from "@solidjs/meta";
 import { createQuery } from "@tanstack/solid-query";
 import { useAuth } from "solid-firebase";
-import { Match, Switch } from "solid-js";
 import { auth } from "~/lib/firebase";
 import { getPillsByUserId } from "~/services/pills";
 import { PillList } from "../components/pills/list";
+import { PillSkeletonList } from "../components/pills/skeleton";
 
 export default function Home() {
 	const { data } = useAuth(auth);
 	const q = createQuery(
 		() => ["pillsByUserId"],
 		async () => await getPillsByUserId(data?.uid ?? ""),
+		{ networkMode: "offlineFirst", cacheTime: 1000 * 60 * 60 * 24 },
 	);
 
 	return (
-		<section class="max-w-3xl  container mx-auto border-2 min-h-screen bg-light-100 h-full w-full  rounded-md  ">
+		<section class="max-w-3xl  container mx-auto border-2 min-h-screen bg-light-100 h-full w-full  rounded-sm  ">
 			<Title>Pills - Home</Title>
 			<h2 class="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0 text-center text-center pt-4">
 				Lista de pastillas actuales
 			</h2>
 			<Switch>
 				<Match when={q.isLoading}>
-					<p>Cargando...</p>
+					<PillSkeletonList />
 				</Match>
 				<Match when={q.isSuccess && q.data && q.data.length !== 0}>
 					<PillList pills={q.data ?? []} />
